5d84768ad06e204a45b46ccd6bd0860d8feda0b5
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2024-11-20 12:59:44 +0100

    Updated build script and README.md + Logo update

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf build
 mkdir -p build
 
 # Compile
-javac -d build src/main/java/com/mrandrej/rpg/*.java src/main/java/com/mrandrej/rpg/entity/*.java src/main/java/com/mrandrej/rpg/map/*.java
+javac -d build src/main/java/com/mrandrej/rpg/*.java src/main/java/com/mrandrej/rpg/entity/*.java src/main/java/com/mrandrej/rpg/map/*.java src/main/java/com/mrandrej/rpg/items/*.java
 
 # Run
 java -cp build main.java.com.mrandrej.rpg.Main
